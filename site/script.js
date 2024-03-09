528205120255b3c2f8f6fb9accbdd03613e7d9e6
alicegdutra <alicegzdutra@gmail.com>
2024-03-09 10:26:30 -0300

    atualizações

--- site/script.js
+++ site/script.js
@@ -14,3 +14,23 @@ function abrirMenu(){
 }
 
 listras.addEventListener("click", abrirMenu);
+
+function abrirSubMenu(){
+    if (this.classList.contains("submenu-active")){
+        this.classList.remove("submenu-active");
+    } else if(menu.querySelector(".submenu-active")){
+        menu.querySelector(".submenu-active").classList.remove("submenu-active");
+        this.classList.add("submenu-active");
+    }
+    
+    else{
+        this.classList.add("submenu-active");
+    }
+}
+
+for (let item of itens){
+    if (item.querySelector(".submenu")){
+        item.addEventListener("click", abrirSubMenu);
+
+    }
+}
